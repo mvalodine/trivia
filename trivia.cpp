1bf74d98df16f8f466ed92a322a852a937a381e1
mvalodine <michellevalodine@gmail.com>
2025-01-19 18:27:31 -0700

    Create trivia.cpp
    My first c++ exercise

---
+++ trivia.cpp
@@ -0,0 +1,25 @@
+#include <iostream>                                     
+ using namespace std;
+
+ int main()
+ {
+	cout << "\t\t\t\tBasic BYU Trivia\n\n";
+
+	cout << "\t\tQuestions" << "\t\t\t\tAnswers\n\n";
+
+	cout << "What was the original name of BYU?" << "\t\tBrigham Young Academy\n";
+
+	cout << "When was BYA established?" << "\t\t\t1875\n";
+
+	cout << "Who was the first \"permanent\" principal of BYA?\tKarl Maeser\n";
+	
+	cout << "To what sports conference do we belong?" << "\t\tWest Coast Conference\n";
+
+	cout << "When did BYU win the national football title?"<< "\t1984\n";
+
+	cout << "Who won the Heisman Trophy in 1990?"<< "\t\tTy Detmer\n";
+
+	system("pause");
+	
+	return 0;
+ }
